3c957a6e251d5460e172558612f78f2030b3324f
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-06 08:34:53 -0700

    add src folder to house the source code for the project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import './style.scss';
+
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = 'Welcome to our Capstone API Project';
+    element.classList.add('hello');
+    return element;
+  }
+
+  document.body.appendChild(component());
