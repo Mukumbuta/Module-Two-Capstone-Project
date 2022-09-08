12a9977b307aa42bd8d79433f1e9c4a7c7ed3ec2
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-08 11:30:03 -0700

    fix linter errors

--- src/modules/requestAPIMovies.js
+++ src/modules/requestAPIMovies.js
@@ -1,6 +1,5 @@
 import renderMovies from './renderMovies.js';
 
-
 const getAPIMovies = async () => {
   const apiURL = 'https://api.tvmaze.com/shows?limit=20';
   const response = await fetch(apiURL);
@@ -8,9 +7,9 @@ const getAPIMovies = async () => {
 
   const movieListSize = 20;
   const movieArr = [];
-  for(let i = 0; i< movies.length; i += movieListSize) {
-    const listSize = movies.slice(i, i + movieListSize);
-    movieArr.push(listSize)
+  for (let i = 0; i < movies.length; i += movieListSize) {
+    const moviesList = movies.slice(i, i + movieListSize);
+    movieArr.push(moviesList);
   }
   renderMovies(movieArr[0]);
 };
