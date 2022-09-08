8fcf368d6d2d905f6a0d40e2c51952456e830cbf
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-08 11:25:21 -0700

    add logic to get only 20 movies from the API

--- src/modules/requestAPIMovies.js
+++ src/modules/requestAPIMovies.js
@@ -1,11 +1,18 @@
 import renderMovies from './renderMovies.js';
 
+
 const getAPIMovies = async () => {
-  const apiURL = 'https://api.tvmaze.com/shows';
+  const apiURL = 'https://api.tvmaze.com/shows?limit=20';
   const response = await fetch(apiURL);
   const movies = await response.json();
 
-  renderMovies(movies);
+  const movieListSize = 20;
+  const movieArr = [];
+  for(let i = 0; i< movies.length; i += movieListSize) {
+    const listSize = movies.slice(i, i + movieListSize);
+    movieArr.push(listSize)
+  }
+  renderMovies(movieArr[0]);
 };
 
 export default getAPIMovies;
