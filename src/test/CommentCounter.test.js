b5db553bcb6cf4965ddb793040d62e2917954638
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-10 13:48:33 -0700

    fix linter errors

--- src/test/CommentCounter.test.js
+++
@@ -1,8 +0,0 @@
-import commentCounter from '../modules/commentCounter.js';
-
-describe('Counting the number of comments on a movie', () => {
-  test('should return the length of an array', () => {
-    const arr = commentCounter('film');
-    expect(arr).toEqual(4);
-  });
-});
