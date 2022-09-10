d5ef18f0a90f41a3e4c3f08521616b472bee0773
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-09 18:49:25 -0700

    fix linter errors

--- src/modules/showLikes.js
+++ src/modules/showLikes.js
@@ -1,8 +1,8 @@
 const displayLikes = (event) => {
-    const iconID = (event.target.id).split('like-movie').slice(1);
-    const targetIcon = document.getElementById(`likes-holder${iconID}`);
-    targetIcon.innerHTML.split(' ');
-    document.getElementById(`likes-holder${iconID}`).innerHTML = `${(parseInt(targetIcon.innerText[0], 10)) + 1} Likes`;
-}
+  const iconID = (event.target.id).split('like-movie').slice(1);
+  const targetIcon = document.getElementById(`likes-holder${iconID}`);
+  targetIcon.innerHTML.split(' ');
+  document.getElementById(`likes-holder${iconID}`).innerHTML = `${(parseInt(targetIcon.innerText[0], 10)) + 1} Likes`;
+};
 
 export default displayLikes;
